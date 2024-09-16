c7f944cb62619397aaca17c830cd5d721704be3a
Shubham <shubham@gmail.com>
2024-09-16 07:54:14 +0530

    Add limitFunctionCallCount function

--- closures/solutions.js
+++ closures/solutions.js
@@ -15,3 +15,30 @@ export function counterFactory() {
 // console.log(count.increment());
 // console.log(count.increment());
 // console.log(count.increment());
+
+
+export function limitFunctionCallCount(callback, n) {
+    let startLimit = 0;
+
+    function limit(...args) {
+        if (startLimit < n){
+            startLimit++;
+            return callback(...args)
+        }
+        else{
+            return null;
+        }
+    }
+    return limit;
+}
+
+// const printing = limitFunctionCallCount((ele) =>{
+//     // console.log("value: ", ele);
+//     return ele;
+    
+// }, 3);
+
+// console.log(printing("shubham"));
+// console.log(printing("tulsi"));
+// console.log(printing("bodalkar"));
+// console.log(printing("mul"));
--- closures/tests.js
+++ closures/tests.js
@@ -1,9 +1,19 @@
-import { counterFactory } from "./solutions.js";
+import { counterFactory, limitFunctionCallCount } from "./solutions.js";
 
 const counter = counterFactory();
 
-console.log(counter.increment());
-console.log(counter.increment());
-console.log(counter.increment());
-console.log(counter.decrement());
-console.log(counter.decrement());
+// console.log(counter.increment());
+// console.log(counter.increment());
+// console.log(counter.increment());
+// console.log(counter.decrement());
+// console.log(counter.decrement());
+
+const printing = limitFunctionCallCount((ele) => {
+    return ele;
+}, 3);
+
+console.log(printing("shubham"));    //shubham
+console.log(printing("tulshidas"));  //tulshidas
+console.log(printing("bodalkar"));   //bodalkar
+console.log(printing("mul"));        //null
+console.log(printing("chandrapur")); //null
